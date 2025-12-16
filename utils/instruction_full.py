8a24fc13587a882389281d1f813dd6d17c000c4d
LiuHeyang0114 <65945703+LiuHeyang0114@users.noreply.github.com>
2025-12-16 14:45:19 +0800

    Update instruction_full.py

--- utils/instruction_full.py
+++ utils/instruction_full.py
@@ -13,11 +13,13 @@ from config import EMOTION2VEC_PLUS_LARGE
 
 warnings.filterwarnings("ignore")
 def find_emotion_id(text):
-    id2emotion = ['生气', '高兴', '中性', '伤心', '惊喜', 'unknown']
+    id2emotion = ['生气', '高兴', '中性', '悲伤', '惊喜', 'unknown']
     text_lower = text.lower() 
     for i, emotion in enumerate(id2emotion):
         if emotion in text_lower:
             return i
+    if '伤心' in text_lower:
+      return 3
     return id2emotion.index('unknown') 
 
 
